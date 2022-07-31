105538d773c5a5ba4d8ae5f9a7c3b65fac2d5fcd
nneddk <meljandre.mc@gmail.com>
2022-07-31 14:44:41 +0800

    redid div placing script to accomodate columns and rows instead of straight array

--- script.js
+++ script.js
@@ -1,108 +1,100 @@
 const gameBoard = document.getElementById('game-board');
 
 const gameBoardTiles = (()=>{
-    let placements = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    let whoseTurn = 0;
-    let turns = 0;
-    let x = [];
-    let o = [];
-    let winstate = 0;
+    let gBoard = [['_', '_', '_'],
+                  ['_', '_', '_'],
+                  ['_', '_', '_']];
+    let player = 'x', opponent = 'o';
+    //checks if the game is over
     let gameOver = false;
+    //checks whose turn it is to move;
+    let whoseTurn = 0;
     //this creates the divs
     const makeTiles = () =>{
-        placements.forEach(element => {
+        //rows creation
+        gBoard.forEach(rows => {
+            //column creation
+            rows.forEach(columns => {
+
             let tileHolder = document.createElement('div');
             gameBoard.appendChild(tileHolder);
-
             let tiles = document.createElement('div');
             tiles.classList.add('tile');
-            //this is a listener to set the text content to x/o depending on whose turn it is.
             let clicked = false;
+            tileHolder.appendChild(tiles);
             tileHolder.onclick = function(){
+                let rCoord = gBoard.indexOf(rows);
+                let cCoord = rows.indexOf(columns);
                 tiles.classList.remove('tile');
                 //can only set it once
-                if(!clicked&&!gameOver){
-                    whoseTurn = ++whoseTurn % 2;
+                if(!clicked){
                     tiles.style.transform = 'scale(0.7)';
-                    tiles.textContent = setTile(element);
-                    turns++;
-                    if (turns == 9&& !winstate){
-                        announceWinner();
-                    }
-                    if(whoseTurn){
-                        setTimeout(() => {
-                            robotTurn();
-                        }, 500);   
-                    }
-                    console.log(turns);
+                    tiles.classList.remove('tile');
+                    whoseTurn = ++whoseTurn % 2;
+                    tiles.textContent = setTile(rCoord,cCoord);
+                    console.log();
+                    let availableTiles = document.querySelectorAll('.tile');
+                    /*if (whoseTurn){
+                        robotTurn();
+                    }*/
+                    
                 }
                 clicked = true;
             }
-            tileHolder.appendChild(tiles);
+            });
+            
         });
     }
+
     const robotTurn = () =>{
-        if(turns!=9){
-        ranTile = document.querySelectorAll('.tile');
-        function getRandomInt() {
-            return Math.floor(Math.random() * ranTile.length);
-          }
-        ranTile[getRandomInt()].click();
-        }
+        let availableTiles = document.querySelectorAll('.tile');
     }
     //setting x/o on tiles on click
-    const setTile = (element) =>{
-        if (whoseTurn){
-            x.push(element);
-            whoWins(x);
-            return 'x';
+    const setTile = (r, c) =>{
+        if(whoseTurn){
+            gBoard[r][c] = player;
+            return player;
         }else{
-            o.push(element);
-            whoWins(o);
-            return 'o';
+            gBoard[r][c] = opponent;
+            return opponent;
         }   
     }
-
-    const whoWins = (checker) => {
-        //horizontal checks, could be done better but we
-        if (checker.includes(0) && checker.includes(1) && checker.includes(2)){
-            winstate++;
-            announceWinner(whoseTurn);
-        }else if(checker.includes(3) && checker.includes(4) && checker.includes(5)){
-            winstate++;
-            announceWinner(whoseTurn);
-        }else if(checker.includes(6) && checker.includes(7) && checker.includes(8)){
-            winstate++;
-            announceWinner(whoseTurn);
-            //vertical checks, could be done better but we
-        }else if(checker.includes(0) && checker.includes(3) && checker.includes(6)){
-            winstate++;
-            announceWinner(whoseTurn);
-        }else if(checker.includes(1) && checker.includes(4) && checker.includes(7)){
-            winstate++;
-            announceWinner(whoseTurn);
-        }else if(checker.includes(2) && checker.includes(5) && checker.includes(8)){
-            winstate++;
-            announceWinner(whoseTurn);
-            //corner checks, could be done better but we
-        }else if(checker.includes(0) && checker.includes(4) && checker.includes(8)){
-            winstate++;
-            announceWinner(whoseTurn);
-        }else if(checker.includes(2) && checker.includes(4) && checker.includes(6)){
-            winstate++;
-            announceWinner(whoseTurn);
+    //
+    const checkBoard = () => {
+        for (let counter = 0; counter < 3; counter++){
+            //check rows
+            if (gBoard[counter][0] == gBoard[counter][1] && gBoard[counter][1] == gBoard[counter][2]){
+                if (gBoard[counter][0] == player)
+                return +10;
+                else if (gBoard[counter][0] == opponent)
+                return -10;
+            }
+            //check columns
+            if (gBoard[0][counter] == gBoard[1][counter] && gBoard[1][counter] == gBoard[2][counter]){
+                if (gBoard[0][counter] == player)
+                return +10;
+                else if (gBoard[0][counter] == opponent)
+                return -10;
+            }
         }
+        //check cross
+        if (gBoard[0][0] == gBoard[1][1] && gBoard[1][1] == gBoard[2][2]){
+            if (gBoard[0][0] == player)
+            return +10;
+            else if (gBoard[0][0] == opponent)
+            return -10;
+            
+        }
+        if (gBoard[0][2] == gBoard[1][1] && gBoard[1][1] == gBoard[2][0]){
+            if (gBoard[0][2] == player)
+            return +10;
+            else if (gBoard[0][2] == opponent)
+            return -10;
+        }
+        return 0;
     }
     const announceWinner = () =>{
-        if(winstate == 0){
-            console.log('Tie');
-        }else{
-            console.log((whoseTurn?'x':'o')+' wins'); 
-        }
-        console.log(winstate);
-        gameOver = true;
     }
-
     return{makeTiles};
 })();
 gameBoardTiles.makeTiles();
