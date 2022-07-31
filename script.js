de924d12a1e7aa007f258c7e49e16270e82b7892
nneddk <meljandre.mc@gmail.com>
2022-07-31 19:24:21 +0800

    added a depth 1 minimax function, no one likes playing if they cant win ;)

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const gameBoardTiles = (()=>{
     let gBoard = [['_', '_', '_'],
                   ['_', '_', '_'],
                   ['_', '_', '_']];
+    let aiBoard = [[0, 1, 2],
+                  [3, 4, 5],
+                  [6, 7, 8]];
     let player = 'x', opponent = 'o';
+    function getRandomInt() {
+        return Math.floor(Math.random() * 3);
+      }
     //checks if the game is over
     let gameOver = false;
     //checks whose turn it is to move;
@@ -12,83 +18,141 @@ const gameBoardTiles = (()=>{
     //this creates the divs
     const makeTiles = () =>{
         //rows creation
-        gBoard.forEach(rows => {
-            //column creation
-            rows.forEach(columns => {
-
+        for (let i = 0; i<3; i++){
+            for (let j = 0; j<3; j++ ){
             let tileHolder = document.createElement('div');
             gameBoard.appendChild(tileHolder);
             let tiles = document.createElement('div');
             tiles.classList.add('tile');
             let clicked = false;
+            let rCoord = i;
+            let cCoord = j;
             tileHolder.appendChild(tiles);
-            tileHolder.onclick = function(){
-                let rCoord = gBoard.indexOf(rows);
-                let cCoord = rows.indexOf(columns);
-                tiles.classList.remove('tile');
+            tileHolder.onclick = function(){    
                 //can only set it once
-                if(!clicked){
+                if(!clicked&&!gameOver){
                     tiles.style.transform = 'scale(0.7)';
-                    tiles.classList.remove('tile');
                     whoseTurn = ++whoseTurn % 2;
-                    tiles.textContent = setTile(rCoord,cCoord);
-                    console.log();
-                    let availableTiles = document.querySelectorAll('.tile');
-                    /*if (whoseTurn){
-                        robotTurn();
-                    }*/
+                    tiles.textContent = setTile(rCoord, cCoord);
+                    
+
+                    
+                    setTimeout(() => {
+                        if (whoseTurn){
+                            
+                            robotRandTurn();
+                        } 
+                    }, 500);
+                    if (checkBoard(gBoard) == 10){
+                        console.log('gameOver');
+                    }else if(checkBoard(gBoard) == -10){
+                        console.log('gameOver');
+                    }
                     
                 }
                 clicked = true;
             }
-            });
-            
-        });
+
+            }
+        }
+        
     }
 
-    const robotTurn = () =>{
-        let availableTiles = document.querySelectorAll('.tile');
+    const robotRandTurn = () =>{
+        
+        let ranTile = document.querySelectorAll('.tile');
+        let randR = getRandomInt();
+        let randC = getRandomInt(); 
+        let aiTile = aiBoard[randR][randC];
+        if (bestMove() != -1){
+            ranTile[bestMove()].click();
+        }else if(aiTile!= '_'){
+            ranTile[aiTile].click();
+        }else{
+            robotRandTurn();
+        }
     }
     //setting x/o on tiles on click
     const setTile = (r, c) =>{
         if(whoseTurn){
             gBoard[r][c] = player;
+            aiBoard[r][c] = '_';
             return player;
         }else{
             gBoard[r][c] = opponent;
+            aiBoard[r][c] = '_';
             return opponent;
         }   
     }
+    //only did 1 depth level for minimax
+    const bestMove = () => {
+        let winActive = false;
+        aiMove = [-1,-1];
+        //see if player is winning
+        for (let i = 0; i<3; i++){
+            for (let j = 0; j<3; j++){
+                if (gBoard[i][j] == '_'){
+                    gBoard[i][j] = opponent;
+                    if (checkBoard(gBoard) == -10){
+                        console.log('o is winning at : '+i+':'+j);
+                        winActive = true;
+                        aiMove[0] = i;
+                        aiMove[1] = j;
+                    }
+                    gBoard[i][j] = '_'
+                }
+                if (gBoard[i][j] == '_'){
+                gBoard[i][j] = player;
+                if (checkBoard(gBoard) == 10){
+                    console.log('x is winning at : '+i+':'+j);
+                    winActive = true;
+                    aiMove[0] = i;
+                    aiMove[1] = j;
+                }
+                gBoard[i][j] = '_'
+                }
+                
+            }
+        }
+        if (winActive){
+            return aiBoard[aiMove[0]][aiMove[1]]; 
+        }
+        else return -1;
+        
+        
+
+        
+    }
     //
-    const checkBoard = () => {
+    const checkBoard = (board) => {
         for (let counter = 0; counter < 3; counter++){
             //check rows
-            if (gBoard[counter][0] == gBoard[counter][1] && gBoard[counter][1] == gBoard[counter][2]){
-                if (gBoard[counter][0] == player)
+            if (board[counter][0] == board[counter][1] && board[counter][1] == board[counter][2]){
+                if (board[counter][0] == player)
                 return +10;
-                else if (gBoard[counter][0] == opponent)
+                else if (board[counter][0] == opponent)
                 return -10;
             }
             //check columns
-            if (gBoard[0][counter] == gBoard[1][counter] && gBoard[1][counter] == gBoard[2][counter]){
-                if (gBoard[0][counter] == player)
+            if (board[0][counter] == board[1][counter] && board[1][counter] == board[2][counter]){
+                if (board[0][counter] == player)
                 return +10;
-                else if (gBoard[0][counter] == opponent)
+                else if (board[0][counter] == opponent)
                 return -10;
             }
         }
-        //check cross
-        if (gBoard[0][0] == gBoard[1][1] && gBoard[1][1] == gBoard[2][2]){
-            if (gBoard[0][0] == player)
+        //check diagonal
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            if (board[0][0] == player)
             return +10;
-            else if (gBoard[0][0] == opponent)
+            else if (board[0][0] == opponent)
             return -10;
             
         }
-        if (gBoard[0][2] == gBoard[1][1] && gBoard[1][1] == gBoard[2][0]){
-            if (gBoard[0][2] == player)
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+            if (board[0][2] == player)
             return +10;
-            else if (gBoard[0][2] == opponent)
+            else if (board[0][2] == opponent)
             return -10;
         }
         return 0;
